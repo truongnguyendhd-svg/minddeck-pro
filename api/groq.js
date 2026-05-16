95a3ad5da22d6431545cc83dc3a4c1f939f8aaea
truongnguyendhd-svg <truong.nguyend.hd@gmail.com>
2026-05-16 13:47:55 +0700

    Update groq.js

--- api/groq.js
+++ api/groq.js
@@ -47,7 +47,7 @@ export default async function handler(req, res) {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                model: "llama3-70b-8192",
+                model: "llama-3.1-8b-instant",
                 messages: [
                     {
                         "role": "system",
