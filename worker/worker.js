957d1739e7b82ba2c4fc33b59d8e7ec6ae3e9862
truongnguyendhd-svg <truong.nguyend.hd@gmail.com>
2026-07-04 13:56:50 +0700

    Update worker.js

--- worker/worker.js
+++ worker/worker.js
@@ -1,6 +1,6 @@
 var __defProp = Object.defineProperty;
 var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
-// worker.js
+// worker.js truong
 var __defProp2 = Object.defineProperty;
 var __name2 = /* @__PURE__ */ __name((target, value) => __defProp2(target, "name", { value, configurable: true }), "__name");
 var __defProp22 = Object.defineProperty;
