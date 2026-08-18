1d0a335a25ef255bb520e0faa92f30805641b2fc
truongnguyendhd-svg <truong.nguyend.hd@gmail.com>
2026-08-18 11:10:52 +0700

    Update worker.js

--- worker/worker.js
+++ worker/worker.js
@@ -1204,7 +1204,7 @@ var worker_default = {
           }
 
           // Extract video ID
-          const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+          const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|shorts\/|live\/)([^#\&\?]*).*/;
           const match = url.match(regExp);
           const videoId = (match && match[2].length === 11) ? match[2] : null;
           if (!videoId) {
